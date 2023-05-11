0950b407bfe9e6f11d08ad3faeab70976ffa693b
riet <adamriet@gmail.com>
2023-05-11 17:35:23 -0600

    added figlet to display employee manager when starting app

---
+++ index.js
@@ -0,0 +1,11 @@
+//Displays the title of the app in the console
+const figlet = require('figlet');
+
+figlet('Employee Manager', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
